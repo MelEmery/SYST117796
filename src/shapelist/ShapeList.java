c894d1b64364236e7115577ac75b9d71ac9b4953
MelEmery <emeryme@sheridancollege.ca>
2022-01-21 12:38:31 -0500

    my first commit

---
+++ src/shapelist/ShapeList.java
@@ -0,0 +1,18 @@
+
+package shapelist;
+
+/**
+ *This class +++ insert class description here+++
+ *
+ * @author Melissa Emery
+ */
+public class ShapeList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       SmallCircle c=new SmallCircle();
+    }
+
+}
---
+++ src/shapelist/SmallCircle.java
@@ -0,0 +1,30 @@
+
+package shapelist;
+
+/**
+ * This class ++ insert class description here++
+ * @author $Melissa Emery
+ */
+public class SmallCircle {
+
+      private int radius;
+    
+    public SmallCircle()
+    {
+        this.radius=4;
+    }
+
+    /**
+     * @return the radius
+     */
+    public int getRadius() {
+        return radius;
+    }
+
+    /**
+     * @param radius the radius to set
+     */
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
